1e8dbd77f99d5f1cdca6f5a8956c3400f1f771c0
Inam05 <inamulhassan.tech@gmail.com>
2024-07-10 12:38:08 +0500

    Updated

---
+++ Chapter_1/data_types.js
@@ -0,0 +1,9 @@
+fullName = "Inam Ul Hassan";     // string type variable contains all the characters value 
+age = 21;   // integer type variable conatins the numeric value also include decimal value
+price = 10.32;    // integer type variable conatins the numeric value also include decimal value
+x = null ;  // null variable contain nothing 
+y = undefined;  // undefined variable contains if we don't initallize the variable 
+isFollow = true ;  // boolean data type to check the thing is true or false
+console.log(typeof fullName);  // typeof operator is used to check the type of variable
+console.log(fullName); // to print something on the console
+console.log(price);
---
+++ Chapter_1/variables.js
@@ -0,0 +1,23 @@
+// there are differnet types of variable like string , number , true/false , etc.
+// string is a type of variable which is used to store text or words
+// number is a type of variable which is used to store numbers
+// boolean is a type of variable which is used to store true or false
+
+//   Rules to declare variables
+
+// • Variable names are case sensitive; "a" & "A" is different.
+// • Only letters, digits, underscore(_) and $ is allowed. (not even space)
+// • Only a letter, underscore() or $ should be 1st character.
+// • Reserved words cannot be variable names
+
+fullname = 'Name'; // space is not allowed.
+console.log(fullname);
+Console = 12;  
+console.log(Console); // this is correct because the first letter is correct.
+
+// there are five different types to give the name of a varaible
+// 1. camelCase    fullName     this is a correct way
+// 2. PascalCase   FullName
+// 3. snake_case   full_name
+// 4. kebab-case   full-name
+// 5. SCREAMING_SNAKE_CASE
