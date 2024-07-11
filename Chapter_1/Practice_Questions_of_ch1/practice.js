81cbb8488fe7559573a9700aac5ac3f742969c0d
Inam05 <inamulhassan.tech@gmail.com>
2024-07-11 22:10:53 +0500

    Practice Questions files have been updated

---
+++ Chapter_1/Practice_Questions_of_ch1/practice.js
@@ -0,0 +1,31 @@
+/*
+Create a const object for a product which is selling on the amazon.
+Create various type of keys for the product to sale it asap.
+*/
+
+const product = {
+    name: 'Smartphone',
+    price: 1000,
+    discount: 10,
+    rating : 4.5,
+    isAvailable: true,
+    isDeal : true,
+}
+
+console.log(product);
+
+/*
+Question 2
+Create a const object called "profile" to store information shown in the picture 2
+*/
+
+const profile = {
+    name: 'bretmanrock',
+    followers : 1800000,
+    following : 2000,
+    posts : 1000,
+    likes : 1000000,
+    isFollow : false,
+}
+
+console.log(profile);
