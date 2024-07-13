a03aa42d6f6a93cb997364a640ffac98440a9059
Inam05 <inamulhassan.tech@gmail.com>
2024-07-13 21:27:28 +0500

    Template literals concept is added

--- Chapter_3/Strings/strings.js
+++ Chapter_3/Strings/strings.js
@@ -10,3 +10,19 @@ console.log(str[0]); // I
 console.log(str[1]); // n
 console.log(str[2]); // a
 console.log(str[3]); // m
+
+// TEMPLATE LITERALS 
+// template literals are enclosed in backticks(`) instead of quotes
+// template literals can contain placeholders
+// placeholders are enclosed in ${} and can be used to insert variables or expressions
+// template literals can be used to create multi-line strings
+
+let sentence = `This is a template literal`;
+console.log(sentence); // This is a template literal
+
+let obj = {
+    item: 'pen',
+    price: 10,
+};
+let sentence2 = `The ${obj.item} costs ${obj.price}`
+console.log(sentence2); // The pen costs 10
