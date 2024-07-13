3ea568099ea3ad4cc8146d5825891b4daad376e6
Inam05 <inamulhassan.tech@gmail.com>
2024-07-13 21:34:00 +0500

    Escape Characters concept is added

--- Chapter_3/Strings/strings.js
+++ Chapter_3/Strings/strings.js
@@ -26,3 +26,16 @@ let obj = {
 };
 let sentence2 = `The ${obj.item} costs ${obj.price}`
 console.log(sentence2); // The pen costs 10
+
+// escape characters
+// escape characters are used to insert special characters into a string
+// they are preceded by a backslash (\)
+// some common escape characters are \n for new line, \t for tab,
+// \r for carriage return, \b for backspace, \f for form feed,
+// \v for vertical tab, \\" for double quote, \\ for backslash
+// \uXXXX for unicode character, \xXX for hexadecimal character
+
+console.log("Inam Ul Hassan \nAge = 20");
+console.log("Inam Ul Hassan \tAge = 20");
+let str_ = "Inam Ul Hassan \tAge = 20";
+console.log(str_.length); // Inam Ul Hassan	 Age = 20
