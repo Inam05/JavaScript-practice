268afbae1fcd8e8ed1fba27e4360e19cf7e35496
Inam05 <inamulhassan.tech@gmail.com>
2024-07-13 14:18:26 +0500

    if-else statements added

--- Chapter_2/Operators_and_Conditional_Statements/conditional_statements.js
+++ Chapter_2/Operators_and_Conditional_Statements/conditional_statements.js
@@ -19,3 +19,35 @@ if (mode === "light") {
 }
 console.log(color);
 
+// if-else statements
+// example 1
+
+let age_ = 17;
+if (age_ > 18) {
+    console.log("You can vote")
+}
+else {
+    console.log("You can't vote")
+}
+
+// example 2
+let mode_ = "light";
+let color_ = "white";
+if (mode_ === "dark") {
+    color_ = "black";
+}
+else {
+    color_ = "white";
+}
+console.log(color_);
+
+// example 3 (odd or even)
+
+let number = 5;
+if (number % 2 === 0) {
+    console.log(number,"is even");
+}
+else {
+    console.log(number,"is odd");
+}
+
