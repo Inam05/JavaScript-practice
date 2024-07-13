20fef9af7bee046cb24562ed5cb5816d597980c1
Inam05 <inamulhassan.tech@gmail.com>
2024-07-13 14:03:08 +0500

    Logical operators added

--- Chapter_2/Operators_and_Conditional_Statements/operators.js
+++ Chapter_2/Operators_and_Conditional_Statements/operators.js
@@ -1,31 +1,32 @@
 // artheimetic operators
 let a = 5;
 let b = 2;
-console.log("a =",a, "& b =", b);
+console.log("a =", a, "& b =", b);
 // addition
-console.log("a + b =",a + b);
+console.log("a + b =", a + b);
 // subtraction
-console.log("a - b =",a - b);
+console.log("a - b =", a - b);
 // multiplication
-console.log("a * b =",a * b);
+console.log("a * b =", a * b);
 // division
-console.log("a / b =",a / b);
+console.log("a / b =", a / b);
 // modulus
-console.log("a % b =",a % b);
+console.log("a % b =", a % b);
 // exponentiation
-console.log("a ** b =",a ** b);  //5^2  power rule
+console.log("a ** b =", a ** b);  //5^2  power rule
 
 
 //Unary operators
-let c =6 ;
+let c = 6;
 let d = 3;
 console.log("++c =", ++c); // pre increment
 console.log("--c =", --c); // pre decrement
 console.log("d++ =", d++); // post increment
 console.log("d-- =", d--); // post decrement
 
+
 //Assignment operators
-let e = 2 ;
+let e = 2;
 console.log("e = 5  ", e = 2); // assignment operator
 // **=
 console.log("e **= 2  ", e **= 2); // e = e
@@ -55,3 +56,17 @@ console.log("f >= g  ", f >= g); // false
 console.log("f <= g  ", f <= g); // true
 console.log("f == g  ", f == g); // false
 console.log("g == h  ", g == h); // true
+
+
+// Logical operators
+let i = 6;
+let j = 7;
+
+let cond1 = i < j;
+let cond2 = i === 5;
+let cond3 = i === 6;
+console.log("cond1 && cond2  ", cond1 && cond2); // false
+console.log("cond1 && cond3  ", cond1 && cond3); // true
+console.log("cond1 || cond2  ", cond1 || cond2); // true
+console.log("cond1 || cond3  ", cond1 || cond3); // true
+console.log("!(cond1)  ", !(cond1)); // false
