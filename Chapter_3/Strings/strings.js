c98a7469a7c0a3e1a111035e5c94f0f23daa78dc
Inam05 <inamulhassan.tech@gmail.com>
2024-07-13 21:20:09 +0500

    Strings concepts have been added

---
+++ Chapter_3/Strings/strings.js
@@ -0,0 +1,12 @@
+// string is a sequence of characters used to represent text
+
+let str = "Inam Ul Hassan";
+
+// inbuit properties
+// length property
+console.log(str.length); // 14
+// string indicies
+console.log(str[0]); // I
+console.log(str[1]); // n
+console.log(str[2]); // a
+console.log(str[3]); // m
