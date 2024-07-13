b2919a41032a4865bef93e8c52ccb3a4ffd5fbd8
Inam05 <inamulhassan.tech@gmail.com>
2024-07-13 17:45:38 +0500

    while loop concept is added

---
+++ Chapter_3/Loops/while_&_do-while_loops.js
@@ -0,0 +1,12 @@
+// while loop 
+// working of while loop
+// 1. condition is checked
+// 2. if condition is true then statement is executed
+// 3. condition is checked again
+// 4. if condition is false then statement is not executed
+
+let i = 10;
+while (i <= 12) { // condition checked
+    console.log("i =", i);  // statement run
+    i++;  // increament 
+}
