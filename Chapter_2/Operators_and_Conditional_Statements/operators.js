8c5a05287362f637e9869a74498ab253c74d0c1d
Inam05 <inamulhassan.tech@gmail.com>
2024-07-13 11:37:12 +0500

    Arthimetic operators added

---
+++ Chapter_2/Operators_and_Conditional_Statements/operators.js
@@ -0,0 +1,18 @@
+// artheimetic operators
+let a = 5;
+let b = 2;
+console.log("a =",a, "& b =", b);
+// addition
+console.log("a + b =",a + b);
+// subtraction
+console.log("a - b =",a - b);
+// multiplication
+console.log("a * b =",a * b);
+// division
+console.log("a / b =",a / b);
+// modulus
+console.log("a % b =",a % b);
+// exponentiation
+console.log("a ** b =",a ** b);  //5^2  power rule
+
+
