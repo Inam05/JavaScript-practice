ca7ffc4262521eee1f8cdb130088c32bbd3da979
Inam05 <inamulhassan.tech@gmail.com>
2024-07-13 19:25:08 +0500

    do-while loop concept is added

--- Chapter_3/Loops/while_&_do-while_loops.js
+++ Chapter_3/Loops/while_&_do-while_loops.js
@@ -10,3 +10,17 @@ while (i <= 12) { // condition checked
     console.log("i =", i);  // statement run
     i++;  // increament 
 }
+
+
+// do while loop
+// working of do while loop
+// 1. statement is executed
+// 2. condition is checked
+// 3. if condition is true then statement is executed
+// 4. condition is checked again
+// 5. if condition is false then statement is not executed
+let j = 11;
+do {
+    console.log("j =", j);
+    j++;
+} while (j <= 12);
