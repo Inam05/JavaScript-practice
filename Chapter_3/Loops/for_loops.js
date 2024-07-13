175eb09cf21d21180a584387ee9134622a805eb8
Inam05 <inamulhassan.tech@gmail.com>
2024-07-13 17:45:13 +0500

    For loop concept added

---
+++ Chapter_3/Loops/for_loops.js
@@ -0,0 +1,34 @@
+// loops are used to execute a piece of code again and again
+// there are 3 types of loops in javaScript
+// 1. for loop
+// 2. while loop
+// 3. do while loop
+// for loop
+// for (initialization; condition; increment/decrement) {
+//     // code to be executed
+// }
+
+for (let i = 1; i <= 5; i++) {
+    console.log("Inam Ul Hassan");  // 5 time execute 
+}
+
+// sum in for loop
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+    sum = sum + i;
+}
+console.log("sum = ",sum); // 1 + 2 + 3 + 4 + 5
+
+// how for loop is working 
+for (let i = 1; i <= 5; i++) {
+    console.log("i =",i );  // 5 time execute
+}
+
+// if we use var as a defining of "i" it will survive and print 6 after the block
+// for (var i = 1; i <= 5; i++) {
+//     console.log("i =",i );    5 time execute
+// }
+// console.log(i);  print 6 
+
+// infinite loop
+// A loop never ends
