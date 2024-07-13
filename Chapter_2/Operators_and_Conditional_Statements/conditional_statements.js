40a4f1650ef0e87b3a291fac75ffc77987a67f1f
Inam05 <inamulhassan.tech@gmail.com>
2024-07-13 14:17:33 +0500

    If statements added

---
+++ Chapter_2/Operators_and_Conditional_Statements/conditional_statements.js
@@ -0,0 +1,21 @@
+// if statements
+// example 1
+let age = 25;
+if (age > 18) {
+    console.log("You can vote")
+}
+if (age < 18) {
+    console.log("You can't vote")
+}
+
+// example 2
+let mode = "dark";
+let color;
+if (mode === "dark") {
+    color = "black";
+}
+if (mode === "light") {
+    color = "white";
+}
+console.log(color);
+
