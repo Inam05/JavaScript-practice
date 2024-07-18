8be7030cc36d341e5ead3c0c6816b2428e070627
Inam05 <inamulhassan.tech@gmail.com>
2024-07-18 23:41:32 +0500

    Pop method concept in an array has been added

--- Chapter_4/arrays_methods.js
+++ Chapter_4/arrays_methods.js
@@ -37,4 +37,7 @@ arr.push(6);  // new element add at the end of the index of an array
 console.log(arr); // [1, 2, 3, 4, 5, 6 ]
 console.log(arr.length);
 
-
+// pop
+arr.pop(); // remove the last element of an array
+console.log(arr); // [1, 2, 3, 4, 5 ]
+console.log(arr.length); // 5
