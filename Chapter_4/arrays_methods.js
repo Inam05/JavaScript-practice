9fc69202de009615ae7896afdd32823487002d67
Inam05 <inamulhassan.tech@gmail.com>
2024-07-18 23:34:31 +0500

    Information related to the different methods of an array have been added in one single comment before the code

---
+++ Chapter_4/arrays_methods.js
@@ -0,0 +1,32 @@
+/* 
+Methods in arrays
+push() - adds an element to the end of an array
+pop() - removes the last element from an array
+unshift() - adds an element to the beginning of an array
+shift() - removes the first element from an array
+splice() - removes elements from anywhere in an array and also adds new elements
+slice() - returns a shallow copy of a portion of an array into a new array object selected
+from start to end (not including end). The original array will not be modified.
+concat() - joins two or more arrays and returns a new array.
+sort() - sorts the elements of an array in place and returns the array.
+reverse() - reverses the order of the elements of an array in place and returns the array
+indexOf() - returns the first index at which a given element can be found in the array,
+or -1 if it is not present. It compares searchElement using strict equality (===).
+lastIndexOf() - returns the last index at which a given element can be found in the array
+includes() - determines whether an array includes a certain value among its entries, returning true or false
+map() - creates a new array with the results of calling a provided function on every element in
+the calling array.
+filter() - creates a new array with all elements that pass the test implemented by the provided function
+reduce() - applies a function against an accumulator and each element in the array (from left to
+right) to reduce it to a single value.
+forEach() - executes a provided function once for each array element.
+every() - tests whether all elements in the array pass the test implemented by the provided function.
+some() - tests whether at least one element in the array passes the test implemented by the provided
+function.
+find() - returns the value of the first element in the provided array that satisfies the provided testing
+function. If no values satisfy the testing function, undefined is returned.
+findIndex() - returns the index of the first element in the provided array that satisfies the provided
+testing function. If no values satisfy the testing function, -1 is returned.
+*/
+
+
