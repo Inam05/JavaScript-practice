4f73ef90355b30f149eafb97dae89b48891104ed
Inam05 <inamulhassan.tech@gmail.com>
2024-07-19 09:01:49 +0500

    Question 3 related to tic-tac-toe game state (nested arrays) has been added with solution

--- Chapter_4/Practice_Questions/practice.js
+++ Chapter_4/Practice_Questions/practice.js
@@ -7,7 +7,7 @@ final : ['july', 'june', 'march', 'august']
 */
 
 let months = ['january', 'july', 'march', 'august'];
-months.splice(0, 2, 'july','june');
+months.splice(0, 2, 'july', 'june');
 console.log(months);
 
 /*
@@ -17,3 +17,13 @@ Return the index of the "javascript" from the given array, if it was reversed.
 */
 let languages = ['C', 'C++', 'html', 'javascript', 'python', 'java', 'c#', 'sql'];
 console.log(languages.reverse().indexOf('javascript'));
+
+/*
+Tic tac toe
+Question 3
+Create a nested array to show the tic tac toe game state.
+*/
+let game = [ ['X', null, 'O'], [null, 'X', null], ['O', null, 'X'] ];
+console.log(game);
+game [0][1] = 'O';
+console.log(game);
