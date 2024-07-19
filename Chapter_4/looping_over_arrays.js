6a77548194824fb1b8e154d3be0cfbe36981b6e7
Inam05 <inamulhassan.tech@gmail.com>
2024-07-19 08:46:48 +0500

    Constant arrays concept has been added

--- Chapter_4/looping_over_arrays.js
+++ Chapter_4/looping_over_arrays.js
@@ -8,3 +8,10 @@ let arr_2 = arr1;  // arr_2 references the same array as arr1
 arr_2.push('c');   // Modifying arr_2 also modifies arr1
 console.log(arr1); // Output: ['a', 'b', 'c'] because arr1 and arr_2 reference the same array
 console.log(arr_2); // Output: ['a', 'b', 'c'] for the same reason
+
+// constant arrays
+const g = 10;
+// g = 9.8; TypeError: Assignment to constant variable.
+const a =[1,2,3];
+a.push(4);
+console.log(a); // we can push the elements in a constant arrays
