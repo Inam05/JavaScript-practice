6a1af4217b92f6285e2ab5562b98a4bfd91fd8cd
Inam05 <inamulhassan.tech@gmail.com>
2024-07-19 08:28:35 +0500

    Question 2 added with solution

--- Chapter_4/Practice_Questions/arrays_methods_practice.js
+++ Chapter_4/Practice_Questions/arrays_methods_practice.js
@@ -10,3 +10,10 @@ let months = ['january', 'july', 'march', 'august'];
 months.splice(0, 2, 'july','june');
 console.log(months);
 
+/*
+Question 2
+Return the index of the "javascript" from the given array, if it was reversed.
+['C', 'C++', 'html', 'javascript', 'python', 'java', 'c#', 'sql']
+*/
+let languages = ['C', 'C++', 'html', 'javascript', 'python', 'java', 'c#', 'sql'];
+console.log(languages.reverse().indexOf('javascript'));
