18a1b4df2ea3bb24f7884cf06a5872997d194c14
Inam05 <inamulhassan.tech@gmail.com>
2024-08-01 21:26:13 +0500

    Question 3 added

--- Chapter_3/Loops/Self_Practice/app.js
+++ Chapter_3/Loops/Self_Practice/app.js
@@ -4,3 +4,6 @@
 // Question 2
 /* Write a while loop to print all even numbers from 0 to 20. */
 
+// Question 3
+/* Given the array arr = ['a', 'b', 'c', 'd'], use a for-in loop to print all indices of the array. */
+
