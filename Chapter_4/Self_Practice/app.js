bd16e2c8a8d19e983ef8e81b07f39c5d9bcbd4d4
Inam05 <inamulhassan.tech@gmail.com>
2024-08-05 21:26:17 +0500

    Solution of 1 added

--- Chapter_4/Self_Practice/app.js
+++ Chapter_4/Self_Practice/app.js
@@ -1,2 +1,17 @@
 // Question 1
 /*  You are given a list of students, each with their scores in multiple subjects. Write a function that calculates the average score for each student. */
+const studentList = [
+    { name: "Alice", scores: [85, 92, 78] },
+    { name: "Bob", scores: [79, 95, 88] },
+    { name: "Charlie", scores: [92, 91, 85] }
+];
+
+function calculateAverages(studentList) {
+    return studentList.map(student => {
+        const totalScores = student.scores.reduce((acc, score) => acc + score, 0);
+        const averageScore = totalScores / student.scores.length;
+        return { name: student.name, average: averageScore.toFixed(2) };
+    });
+}
+
+console.log(calculateAverages(studentList));
