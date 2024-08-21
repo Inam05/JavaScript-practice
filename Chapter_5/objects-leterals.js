d43197a29c6a81c1cddfe404a795b438ff81be8b
Inam05 <inamulhassan.tech@gmail.com>
2024-08-21 15:13:21 +0500

    Simple object created

---
+++ Chapter_5/objects-leterals.js
@@ -0,0 +1,32 @@
+// objects literals
+
+const person = {
+    name: 'John',
+    age: 30,
+    address:
+    {
+        street: '123 Main St',
+        city: 'Anytown',
+        state: 'CA',
+        zip: '12345'
+    }
+}
+
+// TWITTER  POST
+
+const post = {
+    id: 1,
+    text: 'Hello, world!',
+    likes: 10,
+    comments: [
+        { id: 1, text: 'Great post!' },
+        { id: 2, text: 'Thanks!' },
+    ],
+    user: {
+        id: 1,
+        name: 'John',
+        avatar: 'https://example.com/john.jpg',
+    }
+}
+console.log(post);
+
