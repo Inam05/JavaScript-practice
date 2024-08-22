0fa3eb08263cfe7464177f23e21a555ff72f149e
Inam05 <inamulhassan.tech@gmail.com>
2024-08-22 19:10:16 +0500

    files are added and linked with each other

---
+++ Chapter_7/moreFunctions.js
@@ -0,0 +1,12 @@
+const student1 = {
+    name: 'John Doe',
+    age: 20,
+    eng: 45,
+    math: 50,
+    science: 60,
+    getAvg() {
+        let sum = (this.eng + this.math + this.science) / 3;
+        console.log(`${this.name} got avg marks = ${getAvg}`);
+        console.log("inam");
+    }
+}
