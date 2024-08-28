f2b484e61f69170aca37612de75b3c1c44c0c69e
Inam05 <inamulhassan.tech@gmail.com>
2024-08-28 15:21:42 +0500

    getElementByClassName

---
+++ Chapter_9/app.js
@@ -0,0 +1,9 @@
+let smallImgs = document.getElementsByClassName("oldImg");
+for(let i = 0; i<smallImgs.length; i++){
+    console.dir(smallImgs[i].src);
+}
+for(let i = 0; i<smallImgs.length; i++){
+    smallImgs[i].src = "assets/spiderman_img.png";
+    console.log(`value of image no. ${i} is changed.`);
+}
+
