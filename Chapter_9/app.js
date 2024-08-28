f37ccf184cdff3e475429bd833d4b8657e16c25b
Inam05 <inamulhassan.tech@gmail.com>
2024-08-28 15:32:32 +0500

    Querry Selector

--- Chapter_9/app.js
+++ Chapter_9/app.js
@@ -1,9 +1,15 @@
-let smallImgs = document.getElementsByClassName("oldImg");
-for(let i = 0; i<smallImgs.length; i++){
-    console.dir(smallImgs[i].src);
-}
-for(let i = 0; i<smallImgs.length; i++){
-    smallImgs[i].src = "assets/spiderman_img.png";
-    console.log(`value of image no. ${i} is changed.`);
-}
+// get Element by class name
 
+// let smallImgs = document.getElementsByClassName("oldImg");
+// for(let i = 0; i<smallImgs.length; i++){
+//     console.dir(smallImgs[i].src);
+// }
+// for(let i = 0; i<smallImgs.length; i++){
+//     smallImgs[i].src = "assets/spiderman_img.png";
+//     console.log(`value of image no. ${i} is changed.`);
+// }
+
+
+
+// Query Selecters 
+console.dir(document.querySelector('h1'));
