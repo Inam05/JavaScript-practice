4423c5ade80d99a48618430fd75e007a5877ea84
Inam05 <inamulhassan.tech@gmail.com>
2024-09-05 20:28:13 +0500

    mouse ponter button

---
+++ Chapter_10/domEvents.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector("button");
+// console.dir(btn);
+// btn.onclick = function () {
+//     alert("Button clicked");
+// }
+
+function sayHello () {
+    alert("Hello");
+}
+btn.onclick = sayHello;
