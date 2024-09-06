c390775e47c3828004948b4ea9c600819c4c19ba
Inam05 <inamulhassan.tech@gmail.com>
2024-09-06 09:25:31 +0500

    'this' in the Event Listener

--- Chapter_10/domEvents.js
+++ Chapter_10/domEvents.js
@@ -14,24 +14,33 @@
 // });
 
 
-// Activity 
+// // Activity 
 
+// let btn = document.querySelector("button");
+// btn.addEventListener("click", function () {
+//     let h3 = document.querySelector("h3");
+//     let randomColor = getRandomColor();
+//     h3.innerText = randomColor;
+
+//     let div = document.querySelector("div");
+//     div.style.backgroundColor = randomColor;
+//     console.log("Color updated");
+// });
+
+// function getRandomColor () {
+//     let red = Math.floor(Math.random() * 255);
+//     let green = Math.floor(Math.random() * 255);
+//     let blue = Math.floor(Math.random() * 255);
+
+//     let color = `rgb(${red}, ${green}, ${blue})`;
+//     return color;
+// }
+
+
+
+// 'this' in Event Listeners
 let btn = document.querySelector("button");
 btn.addEventListener("click", function () {
-    let h3 = document.querySelector("h3");
-    let randomColor = getRandomColor();
-    h3.innerText = randomColor;
-
-    let div = document.querySelector("div");
-    div.style.backgroundColor = randomColor;
-    console.log("Color updated");
+    console.log(this.innerText);
+    this.style.backgroundColor = "yellow";
 });
-
-function getRandomColor () {
-    let red = Math.floor(Math.random() * 255);
-    let green = Math.floor(Math.random() * 255);
-    let blue = Math.floor(Math.random() * 255);
-
-    let color = `rgb(${red}, ${green}, ${blue})`;
-    return color;
-}
