219035bcd90e92d211820cc5912375837281d2f9
Inam05 <inamulhassan.tech@gmail.com>
2024-09-06 09:12:10 +0500

    Activity added

--- Chapter_10/domEvents.js
+++ Chapter_10/domEvents.js
@@ -1,4 +1,4 @@
-let btn = document.querySelector("button");
+// let btn = document.querySelector("button");
 // console.dir(btn);
 // btn.onclick = function () {
 //     alert("Button clicked");
@@ -9,6 +9,29 @@ let btn = document.querySelector("button");
 // }
 // btn.onclick = sayHello;
 
+// btn.addEventListener("click", function () {
+//     console.log("button clicked");
+// });
+
+
+// Activity 
+
+let btn = document.querySelector("button");
 btn.addEventListener("click", function () {
-    console.log("button clicked");
+    let h3 = document.querySelector("h3");
+    let randomColor = getRandomColor();
+    h3.innerText = randomColor;
+
+    let div = document.querySelector("div");
+    div.style.backgroundColor = randomColor;
+    console.log("Color updated");
 });
+
+function getRandomColor () {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    let color = `rgb(${red}, ${green}, ${blue})`;
+    return color;
+}
