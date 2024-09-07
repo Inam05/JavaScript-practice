31a2324f0eaa932a4e8746a6994320bf08cf6eec
Inam05 <inamulhassan.tech@gmail.com>
2024-09-07 13:44:46 +0500

    API fetch through promises with try and catch

--- Chapter_12/api.js
+++ Chapter_12/api.js
@@ -10,7 +10,27 @@ console.log(validRes.length);
 let student = {
     name: "John",
     age: 20,
-    uni : "superior",
+    uni: "superior",
 };
 let jsonStud = JSON.stringify(student);
 console.log(jsonStud);
+
+// API fetching
+let url = "https://catfact.ninja/fact";
+fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log("data1 = ",data.fact);
+        return fetch(url);
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((data2) => {
+        console.log("data2 = ",data2.fact);
+    })
+    .catch((error) => {
+        console.error(error);
+    })
