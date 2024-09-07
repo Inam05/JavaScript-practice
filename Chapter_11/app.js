d635aa7304af268d12f57f283a225d60db6f9c55
Inam05 <inamulhassan.tech@gmail.com>
2024-09-07 09:43:24 +0500

    Database roughly code example through call back hell

--- Chapter_11/app.js
+++ Chapter_11/app.js
@@ -16,3 +16,39 @@ changeColor("red", 1000, () => {
         });
     });
 });
+
+
+// Database through call back hell 
+
+// h2 = document.querySelector("h2");
+function savetoDb(data, success, failure) {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if (internetSpeed > 4) {
+        success();
+    } else {
+        failure();
+    }
+};
+savetoDb(
+    "apna college",
+    () => {
+        console.log("success: your data was saved");
+        savetoDb(
+            "hello world",
+            () => {
+                console.log("success2: data2 saved");
+                savetoDb("inam", () => {
+                    console.log("success3: data3 saved")
+                }, () => {
+                    console.log("failure3: data3 failed");
+                })
+            },
+            () => {
+                console.log("failure2: weak connection");
+            }
+        );
+    },
+    () => {
+        console.log("failure: weak connection. data not saved");
+    }
+);
